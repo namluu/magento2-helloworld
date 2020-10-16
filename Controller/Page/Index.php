cfc3243ae592f5c7cc5aaf8884e958096328d672
Nam Luu <nam.luu@isobar.com>
2020-10-16 18:28:59 +0700

    update controller - view model

--- Controller/Page/Index.php
+++ Controller/Page/Index.php
@@ -4,49 +4,23 @@ declare(strict_types=1);
 namespace Namluu\Helloworld\Controller\Page;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Result\PageFactory;
-use Magento\Sales\Model\OrderFactory;
-use Namluu\Helloworld\ViewModel\OrderInfo;
 
 class Index implements HttpGetActionInterface
 {
-    /**
-     * @var OrderFactory
-     */
-    protected $_orderFactory;
-
-    /**
-     * @var RequestInterface
-     */
-    private $request;
-
     /**
      * @var PageFactory
      */
     private $pageFactory;
 
-    private $viewModel;
-
     public function __construct(
-        RequestInterface $request,
-        PageFactory $pageFactory,
-        OrderFactory $orderFactory,
-        OrderInfo $viewModel
+        PageFactory $pageFactory
     ) {
-        $this->_orderFactory = $orderFactory;
-        $this->request = $request;
         $this->pageFactory = $pageFactory;
-        $this->viewModel = $viewModel;
     }
 
     public function execute()
     {
-        $orderId = (int) $this->request->getParam('id');
-        $order = $this->_orderFactory->create();
-        $order->load($orderId);
-        $this->viewModel->setOrder($order);
-
         return $this->pageFactory->create();
     }
 }
--- ViewModel/OrderInfo.php
+++ ViewModel/OrderInfo.php
@@ -1,28 +1,41 @@
 <?php
 namespace Namluu\Helloworld\ViewModel;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Sales\Model\OrderFactory;
 class OrderInfo implements ArgumentInterface
 {
     public $data;
 
     public $countryInformationAcquirer;
 
+    /**
+     * @var OrderFactory
+     */
+    protected $orderFactory;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
     public function __construct(
-        \Magento\Directory\Api\CountryInformationAcquirerInterface $countryInformationAcquirer
+        \Magento\Directory\Api\CountryInformationAcquirerInterface $countryInformationAcquirer,
+        RequestInterface $request,
+        OrderFactory $orderFactory
     )
     {
         $this->countryInformationAcquirer = $countryInformationAcquirer;
-    }
-
-
-    public function setOrder($data)
-    {
-        $this->data = $data;
+        $this->orderFactory = $orderFactory;
+        $this->request = $request;
     }
 
     public function getOrder()
     {
-        return $this->data;
+        $orderId = (int) $this->request->getParam('id');
+        $order = $this->orderFactory->create();
+        $order->load($orderId);
+        return $order;
     }
 
     public function getCountryName()
